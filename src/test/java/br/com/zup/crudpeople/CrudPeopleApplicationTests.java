e1bab820522e86d6e214a4ee832def554b8d5178
Thalyta-dev <thalytamaelycorrea1718@gmail.com>
2021-07-29 08:07:11 -0300

    setup

---
+++ src/test/java/br/com/zup/crudpeople/CrudPeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.crudpeople;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudPeopleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
